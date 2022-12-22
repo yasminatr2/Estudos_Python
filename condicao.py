ec70abef34f9526a8c9d1a9ff39a08a8a067acb5
Yasmin Almeida <yasminatr2004@gmail.com>
2022-12-22 15:06:43 -0300

    mais desafios

--- condicao.py
+++ condicao.py
@@ -1,11 +1,16 @@
 
-num=int(input('Digite um numero: '))
-resul=num%2
-if resul==0:
-    print('Esse numero é par')
-elif resul==1:
-    print('Esse numero é impar')   
 
+import random     
+opcoes=['pedra', 'papel', 'tesoura']
+jogador=['pedra', 'papel', 'tesoura']          
+print('Escolha, pedra papel ou tesoura')
+computador=random.choice(opcoes)
 
+if jogador==opcoes and computador=='papel':
+    print('Você perdeu') 
+elif jogador=='pedra' and computador=='pedra':
+    print('Empate!')
+elif jogador=='pedra' and computador=='tesoura':
+    print('Você ganhou')
 
-    
+                   
